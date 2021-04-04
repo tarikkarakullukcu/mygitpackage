a6743d663082137808b623c23ef9d660dc10fd99
tarikkarakullukcu <81263746+tarikkarakullukcu@users.noreply.github.com>
2021-04-04 18:13:03 +0300

    Update MyGitClass.java

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -4,10 +4,11 @@ public class MyGitClass {
 
 	public static void main(String[] args) {
 		System.out.println("Version 1");
-		
 		System.out.println("My second commit");
-		
 		System.out.println("My third commit");
+		System.out.println("My fourth commit");
+		System.out.println("My fifth commit");
+		System.out.println("uzak sunucudaki kodum");
 	}
 
 }
